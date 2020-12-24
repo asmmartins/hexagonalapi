a4f6425fb17ac7c26728e077f0a8c6129894a7d0
Alex Martins <alexsilvamartinsasm@gmail.com>
2020-12-24 14:09:09 -0300

    feat: Incluido o swagger na api.

--- src/api/Core.Api/Startup.cs
+++ src/api/Core.Api/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Core.Api.Configurations.Swagger;
 
 namespace Core.Api
 {
@@ -25,8 +19,8 @@ namespace Core.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddSwagger();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +32,8 @@ namespace Core.Api
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwaggerUserInterface();
 
             app.UseRouting();
 
