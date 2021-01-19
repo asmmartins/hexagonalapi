cc8319209dd8004dfceceb127c627bd8be57ef48
Alex Martins <alex.martins@wg2r.com>
2021-01-19 18:14:39 -0300

    refactor: Analise de código do VS.

--- src/Repositories/Core.Repositories/DataModels/TerminalDataModel.cs
+++ src/Repositories/Core.Repositories/DataModels/TerminalDataModel.cs
@@ -7,6 +7,6 @@ namespace Core.Repositories.DataModels
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Latitude { get; set; }
-        public string Longitude { get; set; }        
+        public string Longitude { get; set; }
     }
 }
